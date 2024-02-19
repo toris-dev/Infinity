1b6306e5add9ce2283f056f5e23766c9cebd6b51
toris <ironjustlikethat@gmail.com>
2024-02-19 22:07:35 +0900

    Feat: viewsRouter 에서 html static 파일 추가

---
+++ routes/viewsRouter.js
@@ -0,0 +1,20 @@
+// import express from 'express';
+// import path from 'path';
+
+const express = require('express');
+const path = require('path');
+
+const viewsRouter = express.Router();
+// views폴더 내의 ${resource} 폴더 내의 모든 파일을 웹에 띄우며,
+// 이 때 ${resource}.html 을 기본 파일로 설정함.
+function serverStatic(resource) {
+  const resourcePath = path.join(__dirname, `../views/${resource}`);
+  const option = { index: `${resource}.html` };
+
+  // express.static 은 express 가 정적인 파일을 제공할 때 쓰는 함수
+  return express.static(resourcePath, option);
+}
+
+viewsRouter.use('/', serverStatic('home'));
+
+module.exports = { viewsRouter };
