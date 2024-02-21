5ff7af6958cfa66b9e69c3ef6673dd46cabd61ff
toris <ironjustlikethat@gmail.com>
2024-02-21 15:19:07 +0900

    Feat: SPA로 변경하기 위해 정적 파일 제공 변경

--- routes/viewsRouter.js
+++ routes/viewsRouter.js
@@ -5,16 +5,10 @@ const express = require('express');
 const path = require('path');
 
 const viewsRouter = express.Router();
-// views폴더 내의 ${resource} 폴더 내의 모든 파일을 웹에 띄우며,
-// 이 때 ${resource}.html 을 기본 파일로 설정함.
-function serverStatic(resource) {
-  const resourcePath = path.join(__dirname, `../views/${resource}`);
-  const option = { index: `${resource}.html` };
 
-  // express.static 은 express 가 정적인 파일을 제공할 때 쓰는 함수
-  return express.static(resourcePath, option);
-}
-
-viewsRouter.use('/', serverStatic('home'));
+// SPA 이기 때문에 index.html 만 제공
+viewsRouter.get('/*', (req, res) => {
+  res.sendFile(path.resolve('views', 'index.html'));
+});
 
 module.exports = { viewsRouter };
