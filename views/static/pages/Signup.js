71e989d1570fcccb41b7f5660b8525499e703c8f
toris <ironjustlikethat@gmail.com>
2024-02-22 11:53:53 +0900

    Design: 로그인, 회원가입 페이지 title 변경

--- views/static/pages/Signup.js
+++ views/static/pages/Signup.js
@@ -3,7 +3,7 @@ import AbstractView from './AbstractView.js';
 export default class extends AbstractView {
   constructor(params) {
     super(params);
-    this.setTitle('Signup');
+    this.setTitle('회원가입');
   }
 
   getCss() {
