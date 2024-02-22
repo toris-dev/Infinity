42043564b67111df7fdd90d52d0539ef0f947e22
toris <ironjustlikethat@gmail.com>
2024-02-22 16:35:03 +0900

    Feat: 존재하지 않는 path에 접근 시 redirect 404 page 추가

--- views/router/routes.js
+++ views/router/routes.js
@@ -1,3 +1,4 @@
+import Error from '../static/pages/Error.js';
 import Home from '../static/pages/Home.js';
 import Login from '../static/pages/Login.js';
 import Signup from '../static/pages/Signup.js';
@@ -5,5 +6,6 @@ import Signup from '../static/pages/Signup.js';
 export const routes = [
   { path: '/', view: Home },
   { path: '/login', view: Login },
-  { path: '/signup', view: Signup }
+  { path: '/signup', view: Signup },
+  { path: '/error', view: Error }
 ];
