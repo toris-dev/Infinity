d519dfa5cb108f6edc51674ac42e7b868c5def20
이고헌 <gohoney22@gmail.com>
2024-02-24 15:11:20 +0900

    Feat: admin API 카테고리 기능 추가
    카테고리 수정 API 미완성
    카테고리 삭제 API 미완성

--- apis/admin.js
+++ apis/admin.js
@@ -110,13 +110,16 @@ router.get(
     res.json(orders);
   })
 );
-router.get('/orders', asyncHandler(async (req, res) => {
-  const orders = await Orders.find({});
-  if (!orders) {
-    throw new Error('주문이 없습니다.');
-  }
-  res.json(orders);
-}));
+router.get(
+  '/orders',
+  asyncHandler(async (req, res) => {
+    const orders = await Orders.find({});
+    if (!orders) {
+      throw new Error('주문이 없습니다.');
+    }
+    res.json(orders);
+  })
+);
 
 /**
  * 작성자 : 이정은
@@ -124,47 +127,58 @@ router.get('/orders', asyncHandler(async (req, res) => {
  * 추가된 상품의 주문 상태를 변경합니다.
  * flag :: (처리전: 상품준비/배송준비 등이 아직 완료가 안된 상태 & 처리중: 상품준비 || 배송 준비중 상태)
  */
-router.put('/orders', asyncHandler(async (req, res) => {
-  const { orderNum } = req.query;
-  const { orderState } = req.body;
+router.put(
+  '/orders',
+  asyncHandler(async (req, res) => {
+    const { orderNum } = req.query;
+    const { orderState } = req.body;
 
-  const order = await Orders.findOne({ _id: orderNum });
-  if (order) {
-    if(!order.orderDeleteDate) {
-      await Orders.updateOne({ _id: orderNum }, { orderState: orderState });
-      const updatedOrder = await Orders.findOne({ _id: orderNum });
-      res.json(updatedOrder);
+    const order = await Orders.findOne({ _id: orderNum });
+    if (order) {
+      if (!order.orderDeleteDate) {
+        await Orders.updateOne({ _id: orderNum }, { orderState: orderState });
+        const updatedOrder = await Orders.findOne({ _id: orderNum });
+        res.json(updatedOrder);
+      } else {
+        throw new Error(
+          '사용자가 주문을 취소했습니다. 주문상태를 수정할 수 없습니다.'
+        );
+      }
     } else {
-      throw new Error('사용자가 주문을 취소했습니다. 주문상태를 수정할 수 없습니다.');
+      // 주문을 찾을 수 없는 경우 에러 처리
+      throw new Error('주문을 찾을 수 없습니다.');
     }
-  } else {
-    // 주문을 찾을 수 없는 경우 에러 처리
-    throw new Error('주문을 찾을 수 없습니다.');
-  }
-}));
+  })
+);
 
 /**
  * 작성자 : 이정은
  * 작성 시작일: 2024.02.23
- * 생성된 주문 정보를 관리자가 삭제합니다. 
+ * 생성된 주문 정보를 관리자가 삭제합니다.
  * flag :: orderDeleteDate에는 관리자가 주문을 삭제하는 현재 시점의 시간값이 들어갑니다.
  */
-router.delete('/orders', asyncHandler(async (req,res) => {
-  const { orderNum } = req.query;
+router.delete(
+  '/orders',
+  asyncHandler(async (req, res) => {
+    const { orderNum } = req.query;
     const order = await Orders.findOne({ _id: orderNum });
 
-    if(order) {
-        if(!order.orderDeleteDate) {
-            await Orders.updateOne({ _id: orderNum }, { orderDeleteDate: Date.now()+ 9*60*60*1000 });
-            const deletedOrder = await Orders.findOne({ _id : orderNum });
-            res.json({ deletedOrder });
-        } else {
-            throw new Error('이미 사용자가 취소한 주문입니다.');
-        }
+    if (order) {
+      if (!order.orderDeleteDate) {
+        await Orders.updateOne(
+          { _id: orderNum },
+          { orderDeleteDate: Date.now() + 9 * 60 * 60 * 1000 }
+        );
+        const deletedOrder = await Orders.findOne({ _id: orderNum });
+        res.json({ deletedOrder });
+      } else {
+        throw new Error('이미 사용자가 취소한 주문입니다.');
+      }
     } else {
-        throw new Error('주문을 찾을 수 없습니다.');
+      throw new Error('주문을 찾을 수 없습니다.');
     }
-}));
+  })
+);
 
 /* ----------------------카테고리 API----------------------------- */
 // 카테고리 추가 API
@@ -197,43 +211,74 @@ router.post(
 );
 
 // 카테고리 수정 API
-router.put('/category/:prodMajorCat/:prodSubCat?', asyncHandler(async (req, res) => {
-  const prodMajorCat = Number(req.params.prodMajorCat);
-  const {prodSubCat} = req.params;
-  const { updateProdMajorCat, updateProdSubCat } = req.body;
-  let prodCat = await ProdCat.find({ prodMajorCat });
-  if (prodCat.length === 0) {
-    throw new Error('카테고리가 없습니다.')
-  }
-  if (!updateProdMajorCat && !updateProdSubCat) {
-    throw new Error('수정할 카테고리를 입력해주세요.')
-  } else if (prodSubCat && !updateProdMajorCat && updateProdSubCat ) {
-     //소분류만 변경
-    await ProdCat.updateOne(
-      { prodMajorCat, "prodSubCats.prodSubCat": prodSubCat},
-      { $set: { "prodSubCats.$.prodSubCat": updateProdSubCat } }
-    );
-    
-  } else if (updateProdMajorCat && !updateProdSubCat) {
-    //대분류만 변경
-    await ProdCat.updateOne({prodMajorCat}, {prodMajorCat: updateProdMajorCat});
+router.put(
+  '/category/:prodMajorCat/:prodSubCat?',
+  asyncHandler(async (req, res) => {
+    const prodMajorCat = Number(req.params.prodMajorCat);
+    const { prodSubCat } = req.params;
+    const { updateProdMajorCat, updateProdSubCat } = req.body;
+    let prodCat = await ProdCat.find({ prodMajorCat });
+    if (prodCat.length === 0) {
+      //지정한 대분류 카테고리가 없는 경우
+      throw new Error('카테고리가 없습니다.');
+    }
+    if (!updateProdMajorCat && !updateProdSubCat) {
+      throw new Error('수정할 카테고리를 입력해주세요.');
+    } else if (prodMajorCat && !prodSubCat && updateProdSubCat) {
+      //소분류 업데이트 정보가 있으나 업데이트할 소분류를 지정하지 않은 경우
+      throw new Error('업데이트할 소분류가 지정되지 않았습니다.');
+    } else if (prodSubCat && !updateProdMajorCat && updateProdSubCat) {
+      //소분류만 변경
+      await ProdCat.updateOne(
+        { prodMajorCat, 'prodSubCats.prodSubCat': prodSubCat },
+        { $set: { 'prodSubCats.$.prodSubCat': updateProdSubCat } }
+      );
+    } else if (updateProdMajorCat && !updateProdSubCat) {
+      //대분류만 변경
+      await ProdCat.updateOne(
+        { prodMajorCat },
+        { prodMajorCat: updateProdMajorCat }
+      );
+    } else {
+      //모두 변경
+      await ProdCat.updateOne(
+        { prodMajorCat, 'prodSubCats.prodSubCat': prodSubCat },
+        {
+          prodMajorCat: updateProdMajorCat,
+          $set: { 'prodSubCats.$.prodSubCat': updateProdSubCat }
+        }
+      );
+    }
+    /**
+     * 수정이 완료 된 후 수정된 카테고리 응답
+     * !!파라미터에 소분류가 없을 경우!! 비동기가 안맞아 빈 배열이 리턴됨
+     * 기능상으로는 동작
+     * 수정 필요
+     */
+    prodCat = await ProdCat.find({ prodMajorCat });
+    res.json(prodCat);
+  })
+);
 
+//카테고리 삭제
+router.delete(
+  '/category/:prodMajorCat/:prodSubCat?',
+  asyncHandler(async (req, res) => {
+    const prodMajorCat = Number(req.params.prodMajorCat);
+    const { prodSubCat } = req.params;
+    if (!prodSubCat) {
+      //대분류 삭제
+      await ProdCat.deleteOne({ prodMajorCat });
+    } else {
+      //소분류 삭제
+      await ProdCat.updateOne(
+        { prodMajorCat: prodMajorCat },
+        { $pull: { prodSubCats: { prodSubCat: prodSubCat } } }
+      );
+    }
+    const prodCat = await ProdCat.find({});
+    res.json(prodCat);
+  })
+);
 
-  } else {
-    //모두 변경
-    await ProdCat.updateOne(
-      { prodMajorCat, "prodSubCats.prodSubCat": prodSubCat},
-      { prodMajorCat: updateProdMajorCat, $set: { "prodSubCats.$.prodSubCat": updateProdSubCat } }
-    );
-
-
-  }
-  /**
-   * 수정이 완료 된 후 수정된 카테고리 응답
-   * !!파라미터에 소분류가 없을 경우!! 비동기가 안맞아 빈 배열이 리턴됨
-   * 기능상으로는 동작
-   * 수정 필요
-   */
-  await res.json(await ProdCat.find({prodMajorCat}));
-}))
 module.exports = router;
