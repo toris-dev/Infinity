3820bbba0c2cbd523b854f8b62e271ec52cc4710
toris <ironjustlikethat@gmail.com>
2024-02-26 14:22:32 +0900

    Fix: 프론트엔드 라우팅 문제 해결

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const apiRouter = require('./routes/api');
-const indexRouter = require('./routes/index');
 
-const { viewsRouter } = require('./routes/viewsRouter');
 const getUserFromJWT = require('./middlewares/get-user-from-jwt');
 const errorHandler = require('./middlewares/error-handler');
 
@@ -16,7 +14,7 @@ require('./passport')();
 
 require('dotenv').config();
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_ID}:${process.env.DB_PW}@infinitydb.uzbnsom.mongodb.net/`,
+  `mongodb+srv://${process.env.DB_ID}:${process.env.DB_PW}@infinitydb.uzbnsom.mongodb.net/`
 );
 mongoose.connection.on('connected', () => {
   console.log('MongoDB Connected');
@@ -30,13 +28,10 @@ app.use('/static', express.static(path.resolve(__dirname, 'views', 'static')));
 app.use(express.static('views'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(viewsRouter);
 app.use('/api', apiRouter);
 
 app.use(passport.initialize());
 app.use(getUserFromJWT);
-
-app.use('/', indexRouter);
 
 app.use(errorHandler);
 
@@ -44,5 +39,7 @@ app.use(errorHandler);
 app.post('/logout', function (req, res, next) {
   return res.cookie('token', '').json({ message: 'done' });
 });
+
+app.use('/', (req, res) => res.sendFile(path.resolve('views', 'index.html')));
 
 module.exports = app;
