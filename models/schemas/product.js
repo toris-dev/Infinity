ce0b689655d13f38b923cd1420227bce60cf5d4d
toris <ironjustlikethat@gmail.com>
2024-02-27 17:17:30 +0900

    Fix: prodImg Schema 삭제

--- models/schemas/product.js
+++ models/schemas/product.js
@@ -1,51 +1,50 @@
 const { Schema } = require('mongoose');
-const prodImg = require('./prodImg');
 const prodSubCategory = require('./prodSubCategory');
 
 const productSchema = new Schema({
   prodName: {
     type: String,
-    required: true,
+    required: true
   },
   prodSubCategory: {
     type: prodSubCategory,
-    required: true,
+    required: true
   },
   prodCost: {
     type: Number,
-    required: true,
+    required: true
   },
   prodContent: {
-    type: String,
+    type: String
   },
   prodImgs: {
-    type: [String],
+    type: [String]
   },
   prodUseYn: {
-    type: Date,
+    type: Date
   },
   prodRegDate: {
     type: Date,
     required: true,
-    default: () => Date.now() + 9 * 60 * 60 * 1000,
+    default: () => Date.now() + 9 * 60 * 60 * 1000
   },
   prodRemains: {
     type: Number,
-    required: true,
+    required: true
   },
   prodSize: {
     type: String,
-    required: true,
+    required: true
   },
   prodColor: {
     type: String,
-    required: true,
+    required: true
   },
   prodCount: {
     type: Number,
     required: true,
-    default: 0,
-  },
+    default: 0
+  }
 });
 
 module.exports = productSchema;
