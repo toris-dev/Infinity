62352338ba548152f03cd1ad339a36dc0089e3b5
toris <ironjustlikethat@gmail.com>
2024-02-27 21:17:23 +0900

    Fix: Input id -> email 변경

--- views/static/js/login.js
+++ views/static/js/login.js
@@ -1,6 +1,7 @@
 import { navigateTo } from '../../router/index.js';
 import { BASE_URI } from './constant/url.js';
 import { getCookie } from './lib/getCookie.js';
+import { emailValidation, passwordValidation } from './lib/validation.js';
 
 export const login = () => {
   // 리다이렉트를 login 을 화면에 뿌리고 나서 home 으로 이동하여서 좀 버벅임이 있다.
@@ -9,31 +10,21 @@ export const login = () => {
     navigateTo(BASE_URI);
   }
   const $LoginBtn = document.querySelector('.btnLogin');
-  const $userInputId = document.querySelector('.inputId');
+  const $userInputEmail = document.querySelector('.inputEmail');
   const $userInputPassword = document.querySelector('.inputPassword');
-
-  // function validateEmail(email) {
-  //   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  //   return emailRegex.test(email);
-  // }
-
-  // function validatePassword(password) {
-  //   return passwordRegex.test(password);
-  // }
-
   $LoginBtn.addEventListener('click', () => {
-    const enteredEmail = $userInputId.value;
+    const enteredEmail = $userInputEmail.value;
     const enteredPassword = $userInputPassword.value;
 
-    // if (!validateEmail(enteredEmail)) {
-    //   alert('이메일이 형식에 맞지 않습니다.');
-    //   return;
-    // }
+    if (!emailValidation(enteredEmail)) {
+      alert('이메일이 형식에 맞지 않습니다.');
+      return;
+    }
 
-    // if (!validatePassword(enteredPassword)) {
-    //   alert('비밀번호는 영문자, 숫자, 특수 기호를 포함해야 합니다');
-    //   return;
-    // }
+    if (!passwordValidation(enteredPassword)) {
+      alert('비밀번호는 영문자, 숫자, 특수 기호를 포함해야 합니다');
+      return;
+    }
     // 암호화를 하기 위해서는 secretKey가 필요한데 접근 불가
     fetch('http://localhost:3000/api/auth', {
       method: 'POST',
