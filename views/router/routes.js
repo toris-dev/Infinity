40fd031a588e2952008aa4ebeffd3035c3d56930
toris <ironjustlikethat@gmail.com>
2024-02-27 23:03:19 +0900

    Fix: mypageEdit.js 추가

--- views/router/routes.js
+++ views/router/routes.js
@@ -2,25 +2,26 @@ import { categoryProducts } from '../static/js/categoryProducts.js';
 import { errorFnc } from '../static/js/errorFnc.js';
 import { home } from '../static/js/home.js';
 import { login } from '../static/js/login.js';
+import { mypage } from '../static/js/mypage.js';
+import { mypageEdit } from '../static/js/mypageEdit.js';
 import { order } from '../static/js/order.js';
 import { orderCompleted } from '../static/js/orderCompleted.js';
 import { payment } from '../static/js/payment.js';
 import { product } from '../static/js/product.js';
 import { shoppingcartPro } from '../static/js/shoppingcartPro.js';
 import { signup } from '../static/js/signup.js';
-import { mypage } from '../static/js/mypage.js';
 import CategoryProducts from '../static/pages/CategoryProducts.js';
 import ErrorPage from '../static/pages/ErrorPage.js';
 import Home from '../static/pages/Home.js';
 import Login from '../static/pages/Login.js';
+import Mypage from '../static/pages/Mypage.js';
+import MypageEdit from '../static/pages/MypageEdit.js';
 import Order from '../static/pages/Order.js';
 import OrderCompleted from '../static/pages/OrderCompleted.js';
 import Payment from '../static/pages/Payment.js';
 import Product from '../static/pages/Product.js';
 import ShoppingcartPro from '../static/pages/ShoppingcartPro.js';
 import Signup from '../static/pages/Signup.js';
-import Mypage from '../static/pages/Mypage.js';
-import MypageEdit from '../static/pages/MypageEdit.js';
 
 export const routes = [
   { Path: '/', View: Home, Script: home },
@@ -37,5 +38,7 @@ export const routes = [
   { Path: '/orderCompleted', View: OrderCompleted, Script: orderCompleted },
   { Path: '/error', View: ErrorPage, Script: errorFnc },
   { Path: '/mypage', View: Mypage, Script: mypage },
+  { Path: '/payment', View: Payment, Script: payment },
+
   { Path: '/mypageEdit', View: MypageEdit, Script: mypageEdit }
 ];
