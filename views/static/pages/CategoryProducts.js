537f7101d6259900542d1adf465d998042fd4fe2
toris <ironjustlikethat@gmail.com>
2024-02-29 10:09:00 +0900

    Feat: 카테고리 constant 열거형 추가

--- views/static/pages/CategoryProducts.js
+++ views/static/pages/CategoryProducts.js
@@ -1,3 +1,4 @@
+import { categorysEnum } from '../js/constant/categorys.js';
 import AbstractView from './AbstractView.js';
 
 // 무한스크롤 구현
@@ -5,51 +6,25 @@ export default class extends AbstractView {
   constructor(params) {
     super(params);
     this.setTitle('categoryProducts');
+    console.log(this.params);
     // data fetching 후 변경 예정
-    this.catetory = [
-      'new',
-      'Best',
-      '1+1',
-      'All',
-      'Outer',
-      'Top',
-      'Bottom',
-      'Shoes',
-      'Acc'
-    ];
 
-    this.title2 = this.extractCategoryFromURL(); // URL에서 카테고리 추출 // Category 선택에 따른 string
     this.count = 12; // 검색 결과에 따른 count
   }
 
-  // URL에서 카테고리 추출하는 함수
-  extractCategoryFromURL() {
-    const categoryKeywords = [
-      'new',
-      'Best',
-      '1+1',
-      'All',
-      'Outer',
-      'Top',
-      'Bottom',
-      'Shoes',
-      'Acc'
-    ];
-    const pathName = window.location.pathname;
-    const matchedCategory = categoryKeywords.find((keyword) =>
-      pathName.includes(`/categorys/${keyword}`)
-    );
-    return matchedCategory ? matchedCategory : 'All';
-  }
-
   async getHtml() {
-    if (!this.catetory.includes(this.params.categoryProducts)) {
-      window.history.pushState(null, '', '/product/error');
+    const enumValues = Object.values(categorysEnum);
+    const paramValues = Object.values(this.params);
+
+    // enum 과 parameter 비교
+    const hasMatch = paramValues.some((value) => enumValues.includes(value));
+    if (!hasMatch) {
+      window.history.pushState(null, '', '/error');
       return '';
     }
     return `
     <div class="titleContainer">
-        <strong class="categoryTitle">${this.title2}</strong>
+        <strong class="categoryTitle">${paramValues}</strong>
     </div>
     <div class="New-arrival">
     <p class="searchCount">검색 결과 ${this.count}개 검색.</p>
