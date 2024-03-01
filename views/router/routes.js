5c927a7efca3210a7cdb340c5e56db09acbeebd8
이고헌 <gohoney22@gmail.com>
2024-03-01 16:01:12 +0900

    Fix: views/routes admin path setting

--- views/router/routes.js
+++ views/router/routes.js
@@ -10,6 +10,12 @@ import { payment } from '../static/js/payment.js';
 import { product } from '../static/js/product.js';
 import { shoppingcartPro } from '../static/js/shoppingcartPro.js';
 import { signup } from '../static/js/signup.js';
+import { admin } from '../static/js/admin.js';
+import { adminManagement } from '../static/js/adminManagement.js';
+import { adminSetting } from '../static/js/adminSetting.js';
+import { adminCategory } from '../static/js/adminCategory.js';
+import { orderEdit } from '../static/js/orderEdit.js';
+
 import CategoryProducts from '../static/pages/CategoryProducts.js';
 import ErrorPage from '../static/pages/ErrorPage.js';
 import Home from '../static/pages/Home.js';
@@ -17,6 +23,7 @@ import Login from '../static/pages/Login.js';
 import Mypage from '../static/pages/Mypage.js';
 import MypageEdit from '../static/pages/MypageEdit.js';
 import Order from '../static/pages/Order.js';
+import OrderEdit from '../static/pages/OrderEdit.js';
 import OrderCompleted from '../static/pages/OrderCompleted.js';
 import Payment from '../static/pages/Payment.js';
 import Product from '../static/pages/Product.js';
@@ -25,7 +32,7 @@ import Signup from '../static/pages/Signup.js';
 import Admin from '../static/pages/Admin.js';
 import AdminManagement from '../static/pages/AdminManagement.js';
 import AdminSetting from '../static/pages/AdminSetting.js';
-// import AdminCategory from '../static/pages/AdminCategory.js';
+import AdminCategory from '../static/pages/AdminCategory.js';
 
 export const routes = [
   { Path: '/', View: Home, Script: home },
@@ -43,14 +50,14 @@ export const routes = [
   { Path: '/error', View: ErrorPage, Script: errorFnc },
   { Path: '/mypage', View: Mypage, Script: mypage },
   { Path: '/payment', View: Payment, Script: payment },
-  { Path: '/mypageEdit', View: MypageEdit, Script: mypageEdit }
-  // { Path: '/orderEdit', View: OrderEdit, Script: orderEdit },
-  // { Path: '/admin', View: Admin, Script: admin },
-  // {
-  //   Path: '/admin/adminManagement',
-  //   View: AdminManagement,
-  //   Script: adminManagement
-  // },
-  // { Path: '/admin/adminSetting', View: AdminSetting, Script: adminSetting },
-  // { Path: '/admin/adminCategory', View: AdminCategory, Script: adminCatrgory }
+  { Path: '/mypageEdit', View: MypageEdit, Script: mypageEdit },
+  { Path: '/orderEdit', View: OrderEdit, Script: orderEdit },
+  { Path: '/admin', View: Admin, Script: admin },
+  {
+    Path: '/admin/adminManagement',
+    View: AdminManagement,
+    Script: adminManagement
+  },
+  { Path: '/admin/adminSetting', View: AdminSetting, Script: adminSetting },
+  { Path: '/admin/adminCategory', View: AdminCategory, Script: adminCategory }
 ];
